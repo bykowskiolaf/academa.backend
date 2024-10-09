9c300096f88cf17be7cd4b146ef3a0b634256f74
Olaf Hubert Bykowski <bykowola@fit.cvut.cz>
2024-10-09 16:11:38 +0200

    Initial commit

---
+++ src/main/java/dev/bykowski/academa/AcademaApplication.java
@@ -0,0 +1,13 @@
+package dev.bykowski.academa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/bykowski/academa/AcademaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bykowski.academa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
